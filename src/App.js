d9dbe60a35caaadc22ff6530c3ad96182f0491ed
AJ <42003122+AJ-7885@users.noreply.github.com>
2019-03-04 00:19:59 +0100

    Add google analytics to the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo                 from './assets/image/logo.png';
 import './App.css';
 
 class App extends Component {
