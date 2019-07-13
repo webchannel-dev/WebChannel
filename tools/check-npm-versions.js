c1b77ff38f15c0db6e9fe91ba320d133f0e3044f
AJ <42003122+AJ-7885@users.noreply.github.com>
2019-07-13 22:51:27 +0200

    update the dependencies and build tools

---
+++ tools/check-npm-versions.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console */
+
+const pkg = require('../package.json');
+
+const STEM = 'dependencies';
+let dirty = 0;
+
+console.log('CHECK: package.json, only exact versions.');
+// load package.json
+Object.keys(pkg)
+    // find dependencies, devDeve... etc
+    .filter(key => key.toLowerCase().includes(STEM))
+    .forEach(key => {
+        // checks each dependency
+        Object.entries(pkg[key]).forEach(([dependency, version]) => {
+            if (version[0] === '^' || version[0] === '~') {
+                console.error(`Found ${dependency} with ${version}`);
+                dirty += 1;
+            }
+        });
+    });
+
+console.log(dirty > 0 ? `FAILED with ${dirty} invalid versions` : 'SUCCESS');
+process.exit(dirty);
