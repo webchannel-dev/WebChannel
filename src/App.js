d6afedee85b915f30b69d33cbddc600e551cfbed
AJ <42003122+AJ-7885@users.noreply.github.com>
2019-07-14 23:08:17 +0200

    Update (#11)
    * Adding npm ignore
    
    * v0.1.6
    
    * update the npm ignore
    
    * v0.1.7
    
    * update the dependencies and build tools

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import logo from './assets/image/logo.png';
 import './App.css';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>Webchannel.dev coming soon....</p>
-                    <p> v-02.05.2019-21:56 </p>
-                </header>
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Webchannel.dev coming soon....</p>
+                <p> v-02.05.2019-21:56 </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
