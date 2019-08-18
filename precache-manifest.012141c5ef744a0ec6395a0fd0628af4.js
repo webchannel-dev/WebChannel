c12c2094e3185ceca59e6f7f332bba6c4099149c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-18 21:42:57 +0000

    Deploy AJ-7885/webchannel to github.com/AJ-7885/webchannel.git:gh-pages

---
+++ precache-manifest.012141c5ef744a0ec6395a0fd0628af4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8201534ea4b43ea821aa96570ce27457",
+    "url": "/index.html"
+  },
+  {
+    "revision": "254fbd1b0cb7bf3e7117",
+    "url": "/static/css/main.ab9a025f.chunk.css"
+  },
+  {
+    "revision": "6326327ab824d9ecccd0",
+    "url": "/static/js/2.8e63c846.chunk.js"
+  },
+  {
+    "revision": "254fbd1b0cb7bf3e7117",
+    "url": "/static/js/main.71e2e84e.chunk.js"
+  },
+  {
+    "revision": "c632f9094b4b18475711",
+    "url": "/static/js/runtime~main.f85476ff.js"
+  },
+  {
+    "revision": "4839d490575102f4fd91232a0050930f",
+    "url": "/static/media/logo.4839d490.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54217fc23e6fb5bb98539610b0ded2d4.js"
+  "/precache-manifest.012141c5ef744a0ec6395a0fd0628af4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
