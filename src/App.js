d5d2455e407ad2ecd7381707d53198efad871bb3
AJ <42003122+AJ-7885@users.noreply.github.com>
2019-08-18 23:17:14 +0200

    update the footer and header (#12)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 import logo from './assets/image/logo.png';
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>Webchannel.dev coming soon....</p>
                 <p> v-02.05.2019-21:56 </p>
             </header>
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <header>
+            <h1>Web Channel .DEV</h1>
+        </header>
+    );
+};
