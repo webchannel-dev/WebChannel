eabdb44eb0cb124907fad2f71d0150675aac1997
AJ <42003122+AJ-7885@users.noreply.github.com>
2020-07-22 08:40:45 +0200

    test 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import logo from './assets/image/logo.png';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import logo from "./assets/image/logo.png";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
+  return (
+    <div className="App">
+      {/* <Header />
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>Webchannel.dev coming soon....</p>
                 <p> v-19.05.2020-21:56 </p>
             </header>
-            <Footer />
-        </div>
-    );
+            <Footer /> */}
+    </div>
+  );
 }
 
 export default App;
